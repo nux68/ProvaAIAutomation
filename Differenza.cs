b74b562a3e8e00a318be44a921b6d2db02ae5b2a
nux68 <37836356+nux68@users.noreply.github.com>
2026-06-10 12:01:26 +0200

    Aggiunto metodo per calcolare la differenza tra due numeri (issue #7)

---
+++ Differenza.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ProvaAIAutomation
+{
+    /// <summary>
+    /// Classe che fornisce il metodo per calcolare la differenza tra due numeri.
+    /// </summary>
+    public static class Differenza
+    {
+        /// <summary>
+        /// Restituisce la differenza tra due numeri interi.
+        /// </summary>
+        /// <param name="a">Il primo numero intero (minuendo).</param>
+        /// <param name="b">Il secondo numero intero (sottraendo).</param>
+        /// <returns>La differenza a - b.</returns>
+        public static int Calcola(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
