21d04ca26c44f8bebf4e345700dab69c164b6b74
nux68 <37836356+nux68@users.noreply.github.com>
2026-06-10 12:04:37 +0200

    Aggiunto metodo Somma.Calcola per somma di due numeri interi (issue #2)

---
+++ Somma.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ProvaAIAutomation
+{
+    /// <summary>
+    /// Classe che fornisce il metodo per calcolare la somma di due numeri.
+    /// </summary>
+    public static class Somma
+    {
+        /// <summary>
+        /// Restituisce la somma di due numeri interi.
+        /// </summary>
+        /// <param name="a">Il primo numero intero.</param>
+        /// <param name="b">Il secondo numero intero.</param>
+        /// <returns>La somma a + b.</returns>
+        public static int Calcola(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
