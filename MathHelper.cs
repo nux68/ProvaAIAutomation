b908af93f814a74fa22308f180d7c913891ccd2b
nux68 <37836356+nux68@users.noreply.github.com>
2026-06-10 12:10:32 +0200

    Aggiunto metodo Differenza in MathHelper.cs (issue #7)

---
+++ MathHelper.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace ProvaAIAutomation
+{
+    /// <summary>
+    /// Helper per operazioni matematiche di base.
+    /// </summary>
+    public static class MathHelper
+    {
+        /// <summary>
+        /// Calcola la differenza tra due numeri interi.
+        /// </summary>
+        /// <param name="a">Minuendo.</param>
+        /// <param name="b">Sottraendo.</param>
+        /// <returns>La differenza a - b.</returns>
+        public static int Differenza(int a, int b)
+        {
+            return a - b;
+        }
+    }
+}
